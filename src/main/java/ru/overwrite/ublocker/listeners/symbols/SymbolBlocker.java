22e96eebab4f29df669b523708711195cd6cd406
OverwriteMC <artyom030803@gmail.com>
2025-11-23 11:14:54 +0300

    make searchList for symbols static
    we don't want to duplicate it for every class

--- src/main/java/ru/overwrite/ublocker/listeners/symbols/SymbolBlocker.java
+++ src/main/java/ru/overwrite/ublocker/listeners/symbols/SymbolBlocker.java
@@ -21,7 +21,7 @@ public abstract class SymbolBlocker implements Listener {
     protected final Config pluginConfig;
     private final Runner runner;
 
-    private final String[] searchList = {"%player%", "%world%", "%msg%", "%symbol%",};
+    private static final String[] searchList = {"%player%", "%world%", "%msg%", "%symbol%",};
 
     protected SymbolBlocker(UniversalBlocker plugin) {
         this.plugin = plugin;
